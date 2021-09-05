db018435bdb292bb864d2b51f0e851033a2635db
giteron <85683957+giteron@users.noreply.github.com>
2021-09-05 23:18:03 +0100

    complete lessons 1-6

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,70 @@
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+});
 
 export default function Create() {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      Create page
-    </div>
+    <Container>
+      <Typography
+      // className={classes.title}
+      variant="h6"
+      color="textSecondary"
+      component="h2"
+      gutterBottom>
+        Create a new note
+      </Typography>
+
+      <Button
+      // className={classes.btn}
+      onClick={() => console.log('you clicked me')}
+      type="submit"
+      color="secondary"
+      variant="contained"
+      endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+
+      {/* <Button type="submit">Submit</Button>
+      <Button 
+      type="submit" 
+      color="secondary" 
+      variant="outlined"
+      >Submit</Button>
+
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+      {/* <br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color="secondary" fontSize="large" />
+      <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+      <AcUnitOutlinedIcon color="action" fontSize="small" />
+      <AcUnitOutlinedIcon color="error" fontSize="small" />
+      <AcUnitOutlinedIcon color="disabled" fontSize="small" /> */}
+
+    </Container>
   )
 }
